7b82a951e4b9d7d6f26469eaa18ec2239c62310b
BOTAKOP <botakop88-cpu@users.noreply.github.com>
2026-07-30 13:07:48 +0700

    clean: remove internal docs, scripts, and screenshots

--- deploy.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\User\Downloads\SISIMPAN
-npx vercel deploy --token YOUR_VERCEL_TOKEN --prod
--- print-env.cjs
+++
@@ -1,2 +0,0 @@
-console.log('ANON_KEY=' + process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-console.log('URL=' + process.env.NEXT_PUBLIC_SUPABASE_URL);
